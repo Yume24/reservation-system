06094544c3163fdf6467d7e181ce79788e6b07ef
Yume24 <antonijaros04@gmail.com>
2026-01-04 19:10:59 +0100

    add initial Flyway migration script for creating base tables

---
+++ src/main/resources/db/migration/V0__create_init_tables.sql
@@ -0,0 +1,25 @@
+create table Users
+(
+    id       int unsigned auto_increment
+        primary key,
+    email    varchar(50)      not null,
+    name     varchar(50)      not null,
+    surname  varchar(50)      not null,
+    password varchar(255)     not null,
+    role_id     tinyint unsigned not null
+);
+
+create table Roles
+(
+    id   tinyint unsigned auto_increment
+        primary key,
+    role varchar(50) not null
+);
+
+create table Rooms
+(
+    id       int unsigned auto_increment
+        primary key,
+    name     varchar(50)       not null,
+    capacity smallint unsigned null
+);
