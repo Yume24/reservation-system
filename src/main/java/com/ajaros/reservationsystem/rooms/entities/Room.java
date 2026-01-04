1431ed6dc39b0a89982ba6da22c095f089cdde56
Yume24 <antonijaros04@gmail.com>
2026-01-04 19:44:25 +0100

    add JPA entity classes for Role, Room, and User

---
+++ src/main/java/com/ajaros/reservationsystem/rooms/entities/Room.java
@@ -0,0 +1,26 @@
+package com.ajaros.reservationsystem.rooms.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+
+}
---
+++ src/main/java/com/ajaros/reservationsystem/users/entities/User.java
@@ -0,0 +1,39 @@
+package com.ajaros.reservationsystem.users.entities;
+
+import com.ajaros.reservationsystem.roles.entities.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+
+}
