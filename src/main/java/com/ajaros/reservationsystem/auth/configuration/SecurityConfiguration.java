4f23c793d583c376de286f1707b1ca17c3b6a337
Yume24 <antonijaros04@gmail.com>
2026-01-05 23:24:51 +0100

    refactor: simplify `SecurityConfiguration` by removing unused beans and streamlining `AuthenticationManager` setup

--- src/main/java/com/ajaros/reservationsystem/auth/configuration/SecurityConfiguration.java
+++ src/main/java/com/ajaros/reservationsystem/auth/configuration/SecurityConfiguration.java
@@ -6,20 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -37,11 +33,6 @@ public class SecurityConfiguration {
   @Bean
   public JwtDecoder jwtDecoder() {
     return NimbusJwtDecoder.withSecretKey(jwtConfiguration.getSecretKey()).build();
-  }
-
-  @Bean
-  JwtEncoder jwtEncoder() {
-    return NimbusJwtEncoder.withSecretKey(jwtConfiguration.getSecretKey()).build();
   }
 
   @Bean
@@ -68,16 +59,7 @@ public class SecurityConfiguration {
   }
 
   @Bean
-  public DaoAuthenticationProvider daoAuthenticationProvider(
-      UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-    DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-    provider.setPasswordEncoder(passwordEncoder);
-    return provider;
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(
-      DaoAuthenticationProvider daoAuthenticationProvider) {
-    return new ProviderManager(daoAuthenticationProvider);
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+    return config.getAuthenticationManager();
   }
 }
