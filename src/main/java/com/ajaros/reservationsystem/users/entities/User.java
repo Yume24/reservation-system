d35d5f2ff1dfb3e5071fc74feee346f8ca9988b9
Yume24 <antonijaros04@gmail.com>
2026-01-07 20:13:54 +0100

    refactor: update `id` fields in `Room` and `User` entities from `long` to `Long` for nullable support

--- src/main/java/com/ajaros/reservationsystem/users/entities/User.java
+++ src/main/java/com/ajaros/reservationsystem/users/entities/User.java
@@ -20,7 +20,7 @@ public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id")
-  private long id;
+  private Long id;
 
   @Column(name = "email")
   private String email;
