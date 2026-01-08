24c8043f82b66188c313d583a3f1c24e6b87f7f1
Yume24 <antonijaros04@gmail.com>
2026-01-08 20:38:20 +0100

    refactor: simplify `HashUtility` by removing shared `MessageDigest` instance and improving exception handling

--- src/main/java/com/ajaros/reservationsystem/auth/utils/HashUtility.java
+++ src/main/java/com/ajaros/reservationsystem/auth/utils/HashUtility.java
@@ -1,22 +1,18 @@
 package com.ajaros.reservationsystem.auth.utils;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class HashUtility {
-  private static final MessageDigest digest;
-
-  static {
-    try {
-      digest = MessageDigest.getInstance("SHA-256");
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
   public static String hash(String stringToHash) {
-    var hash = digest.digest(stringToHash.getBytes());
-    return Base64.getEncoder().encodeToString(hash);
+    try {
+      var digest = MessageDigest.getInstance("SHA-256");
+      var hash = digest.digest(stringToHash.getBytes(StandardCharsets.UTF_8));
+      return Base64.getEncoder().encodeToString(hash);
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("SHA-256 algorithm not found", e);
+    }
   }
 }
