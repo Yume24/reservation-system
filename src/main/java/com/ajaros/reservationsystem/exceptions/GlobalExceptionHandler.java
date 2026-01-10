e782d196f7d768bb12b8b5ecfe796f1c361e00f6
Yume24 <antonijaros04@gmail.com>
2026-01-10 09:13:40 +0100

    feat: add handler for `HttpMessageNotReadableException` in `GlobalExceptionHandler` to improve request validation responses

--- src/main/java/com/ajaros/reservationsystem/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ajaros/reservationsystem/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +23,10 @@ public class GlobalExceptionHandler {
         .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
     return ResponseEntity.badRequest().body(errors);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<Void> handleHttpMessageNotReadableException() {
+    return ResponseEntity.badRequest().build();
   }
 }
