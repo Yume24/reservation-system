8f9f450e8882ffefcbd6ddff1065d8a9ee25f89e
Antoni Jaros <antonijaros04@gmail.com>
2026-01-16 21:16:46 +0100

    simplify EquipmentResponse formatting by merging annotations into a single line

--- src/main/java/com/ajaros/reservationsystem/equipment/dtos/EquipmentResponse.java
+++ src/main/java/com/ajaros/reservationsystem/equipment/dtos/EquipmentResponse.java
@@ -3,5 +3,4 @@ package com.ajaros.reservationsystem.equipment.dtos;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record EquipmentResponse(
-    @Schema(example = "1") Long id,
-    @Schema(example = "Projector") String name) {}
+    @Schema(example = "1") Long id, @Schema(example = "Projector") String name) {}
