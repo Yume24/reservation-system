7333928b1692dc844096032d6a905f69f6e67aed
Antoni Jaros <ajaros@general-index.com>
2026-01-25 19:53:07 +0100

    Remove CORS configuration and related properties; simplify security setup.

--- backend/src/main/java/com/ajaros/reservationsystem/auth/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/ajaros/reservationsystem/auth/configuration/SecurityConfiguration.java
@@ -1,15 +1,12 @@
 package com.ajaros.reservationsystem.auth.configuration;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,9 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,9 +24,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
   private final JwtConfiguration jwtConfiguration;
-
-  @Value("${cors.allowed-origins}")
-  private List<String> allowedOrigins;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -55,7 +46,6 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) {
     sessionManagementConfig(http);
     csrfConfig(http);
-    corsConfig(http);
     authorizeRequestsConfig(http);
     oauth2ResourceServerConfig(http);
     exceptionHandlingConfig(http);
@@ -66,20 +56,6 @@ public class SecurityConfiguration {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
     return config.getAuthenticationManager();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    var config = new CorsConfiguration();
-    config.setAllowedOrigins(allowedOrigins);
-    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-    config.setAllowedHeaders(List.of("*"));
-    config.setAllowCredentials(true);
-
-    var configSource = new UrlBasedCorsConfigurationSource();
-    configSource.registerCorsConfiguration("/**", config);
-
-    return configSource;
   }
 
   private void sessionManagementConfig(HttpSecurity http) {
@@ -116,9 +92,5 @@ public class SecurityConfiguration {
           c.accessDeniedHandler(
               ((_, response, _) -> response.setStatus(HttpStatus.FORBIDDEN.value())));
         });
-  }
-
-  private void corsConfig(HttpSecurity http) {
-    http.cors(Customizer.withDefaults());
   }
 }
